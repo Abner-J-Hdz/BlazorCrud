1c4714e5f12063ab9ee2120e0f62b074242d0d8d
abner <abner15jh@gmail.com>
2022-12-21 00:48:57 -0600

    Agregar archivos de proyecto.

---
+++ BlazorCrud/Data/BookService.cs
@@ -0,0 +1,60 @@
+﻿using BlazorCrud.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Data
+{
+    public class BookService : IBookService
+    {
+        private readonly BookContext Context;
+
+        public BookService(BookContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<bool> DeleteBook(int id)
+        {
+            var book = await Context.Books.FindAsync(id);
+
+            Context.Books.Remove(book);
+
+            return await Context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<IEnumerable<Books>> GetAllBooks()
+        {
+            return await Context.Books.ToListAsync();
+        }
+
+        public async Task<Books> GetBooksDetails(int id)
+        {
+            return await Context.Books.FindAsync(id);
+        }
+
+        public async Task<bool> InsertBook(Books book)
+        {
+            Context.Books.Add(book);
+
+            return await Context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> SaveBook(Books book)
+        {
+            if (book.BookId > 0)
+                return await UpdateBook(book);
+
+            return await InsertBook(book);
+        }
+
+        public async Task<bool> UpdateBook(Books book)
+        {
+            Context.Entry(book).State = EntityState.Modified;
+
+            return await Context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ BlazorCrud/Data/IBookService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Data
+{
+    interface IBookService
+    {
+        Task<IEnumerable<Books>> GetAllBooks();
+
+        Task<Books> GetBooksDetails(int id);
+
+        Task<bool> InsertBook(Books book);
+
+        Task<bool> UpdateBook(Books book);
+
+        Task<bool> DeleteBook(int id);
+
+        Task<bool> SaveBook(Books book);
+    }
+}
